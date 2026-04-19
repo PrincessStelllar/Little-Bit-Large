1728ae5b38f1ecf6961001930c13b67eec9c2970
titop54 <32750608+Titop54@users.noreply.github.com>
2026-04-19 18:59:37 +0200

    Update tags.js
    Someone ~~not me~~ forgot something

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -206,3 +206,4 @@ ServerEvents.tags('block', catalyst => {
 /* 
 This script is property of Catalyst Studios for use in the modpack Little Bit Large. It is under the All Rights Reserved license.
 It cannot be used or modified outside of Catalyst Studios without explicit permission from Catalyst Studios.
+*/
