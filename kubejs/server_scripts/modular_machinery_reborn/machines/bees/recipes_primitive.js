a6f75ebed27afcd53dc061b2d04108e95ad7836a
Titop54 <32750608+Titop54@users.noreply.github.com>
2026-05-13 16:53:45 +0200

    Primitive Apiary (only produce honey)

---
+++ kubejs/server_scripts/modular_machinery_reborn/machines/bees/recipes_primitive.js
@@ -0,0 +1,25 @@
+/* 
+This script is property of Catalyst Studios for use in the modpack Little Bit Large. It is under the All Rights Reserved license.
+It cannot be used or modified outside of Catalyst Studios without explicit permission from Catalyst Studios.
+*/
+
+ServerEvents.recipes(catalyst => {
+
+    catalyst.recipes.modular_machinery_reborn.machine_recipe("mmr:primitive_apiary", 600)
+        .width(120)
+        .height(60)
+        .progressData(ProgressData.create().x(45).y(23))
+        .produceItem(Item.of('minecraft:honey_bottle'), 0.5)
+        .produceItem(Item.of('minecraft:honeycomb', 2), 0.75)
+        .jei()
+        .produceItem(Item.of('minecraft:honey_bottle'), 0.5, 85, 12) 
+        .produceItem(Item.of('minecraft:honeycomb'), 0.75, 85, 34)
+        .id(`catalyst:mmr/basic_apiary`);
+
+    console.log("[CatJS] Finished Primitive Apiary recipes")
+});
+
+/* 
+This script is property of Catalyst Studios for use in the modpack Little Bit Large. It is under the All Rights Reserved license.
+It cannot be used or modified outside of Catalyst Studios without explicit permission from Catalyst Studios.
+*/
