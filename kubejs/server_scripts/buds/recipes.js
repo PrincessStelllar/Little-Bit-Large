b43dd4bd063560cb1d5c80b0ce8f0c0d6ff76175
Titop54 <32750608+Titop54@users.noreply.github.com>
2026-06-06 18:51:55 +0200

    Im a genious, ik

--- kubejs/server_scripts/buds/recipes.js
+++ kubejs/server_scripts/buds/recipes.js
@@ -7,72 +7,67 @@ It cannot be used or modified outside of Catalyst Studios without explicit permi
 ServerEvents.recipes(catalyst => {
 
     let clusterMapping = {
-        'minecraft:amethyst_cluster': 'minecraft:amethyst_shard',
-        'pastel:topaz_cluster': 'pastel:topaz_shard',
-        'pastel:citrine_cluster': 'pastel:citrine_shard',
-        'pastel:onyx_cluster': 'pastel:onyx_shard',
-        'pastel:moonstone_cluster': 'pastel:moonstone_shard',
+        'pastel:topaz_block': 'pastel:topaz_shard',
+        'pastel:citrine_block': 'pastel:citrine_shard',
+        'pastel:onyx_block': 'pastel:onyx_shard',
+        'pastel:moonstone_block': 'pastel:moonstone_shard',
 
-        'geore:coal_cluster': 'geore:coal_shard',
-        'geore:copper_cluster': 'geore:copper_shard',
-        'geore:diamond_cluster': 'geore:diamond_shard',
-        'geore:emerald_cluster': 'geore:emerald_shard',
-        'geore:gold_cluster': 'geore:gold_shard',
-        'geore:iron_cluster': 'geore:iron_shard',
-        'geore:lapis_cluster': 'geore:lapis_shard',
-        'geore:quartz_cluster': 'geore:quartz_shard',
-        'geore:redstone_cluster': 'geore:redstone_shard',
-        'geore:ancient_debris_cluster': 'geore:ancient_debris_shard',
-        'geore:ruby_cluster': 'geore:ruby_shard',
-        'geore:sapphire_cluster': 'geore:sapphire_shard',
-        'geore:topaz_cluster': 'geore:topaz_shard',
-        'geore:zinc_cluster': 'geore:zinc_shard',
-        'geore:uraninite_cluster': 'geore:uraninite_shard',
-        'geore:black_quartz_cluster': 'geore:black_quartz_shard',
-        'geore:monazite_cluster': 'geore:monazite_shard',
-        'geore:aluminum_cluster': 'geore:aluminum_shard',
-        'geore:lead_cluster': 'geore:lead_shard',
-        'geore:nickel_cluster': 'geore:nickel_shard',
-        'geore:osmium_cluster': 'geore:osmium_shard',
-        'geore:platinum_cluster': 'geore:platinum_shard',
-        'geore:silver_cluster': 'geore:silver_shard',
-        'geore:tin_cluster': 'geore:tin_shard',
-        'geore:tungsten_cluster': 'geore:tungsten_shard',
-        'geore:uranium_cluster': 'geore:uranium_shard',
+        'geore:coal_block': 'geore:coal_shard',
+        'geore:copper_block': 'geore:copper_shard',
+        'geore:diamond_block': 'geore:diamond_shard',
+        'geore:emerald_block': 'geore:emerald_shard',
+        'geore:gold_block': 'geore:gold_shard',
+        'geore:iron_block': 'geore:iron_shard',
+        'geore:lapis_block': 'geore:lapis_shard',
+        'geore:quartz_block': 'geore:quartz_shard',
+        'geore:redstone_block': 'geore:redstone_shard',
+        'geore:ancient_debris_block': 'geore:ancient_debris_shard',
+        'geore:ruby_block': 'geore:ruby_shard',
+        'geore:sapphire_block': 'geore:sapphire_shard',
+        'geore:topaz_block': 'geore:topaz_shard',
+        'geore:zinc_block': 'geore:zinc_shard',
+        'geore:uraninite_block': 'geore:uraninite_shard',
+        'geore:black_quartz_block': 'geore:black_quartz_shard',
+        'geore:monazite_block': 'geore:monazite_shard',
+        'geore:aluminum_block': 'geore:aluminum_shard',
+        'geore:lead_block': 'geore:lead_shard',
+        'geore:nickel_block': 'geore:nickel_shard',
+        'geore:osmium_block': 'geore:osmium_shard',
+        'geore:platinum_block': 'geore:platinum_shard',
+        'geore:silver_block': 'geore:silver_shard',
+        'geore:tin_block': 'geore:tin_shard',
+        'geore:tungsten_block': 'geore:tungsten_shard',
+        'geore:uranium_block': 'geore:uranium_shard',
 
-        'pastel:bismuth_cluster': 'pastel:bismuth_crystal',
-        'pastel:malachite_cluster': 'pastel:pure_malachite',
-        'pastel:azurite_cluster': 'pastel:pure_azurite',
-        'pastel:bloodstone_cluster': 'pastel:pure_bloodstone',
-        'pastel:coal_cluster': 'pastel:pure_coal',
-        'pastel:copper_cluster': 'pastel:pure_copper',
-        'pastel:iron_cluster': 'pastel:pure_iron',
-        'pastel:gold_cluster': 'pastel:pure_gold',
-        'pastel:lapis_cluster': 'pastel:pure_lapis',
-        'pastel:redstone_cluster': 'pastel:pure_redstone',
-        'pastel:diamond_cluster': 'pastel:pure_diamond',
-        'pastel:emerald_cluster': 'pastel:pure_emerald',
-        'pastel:prismarine_cluster': 'pastel:pure_prismarine',
-        'pastel:quartz_cluster': 'pastel:pure_quartz',
-        'pastel:glowstone_cluster': 'pastel:pure_glowstone',
-        'pastel:zinc_cluster': 'pastel:pure_zinc',
-        'pastel:netherite_scrap_cluster': 'pastel:pure_netherite_scrap',
-        'pastel:echo_cluster': 'pastel:pure_echo',
+        'pastel:bismuth_block': 'pastel:bismuth_crystal',
+        'pastel:malachite_block': 'pastel:pure_malachite',
+        'pastel:azurite_block': 'pastel:pure_azurite',
+        'pastel:bloodstone_block': 'pastel:pure_bloodstone',
+        'pastel:coal_block': 'pastel:pure_coal',
+        'pastel:copper_block': 'pastel:pure_copper',
+        'pastel:iron_block': 'pastel:pure_iron',
+        'pastel:gold_block': 'pastel:pure_gold',
+        'pastel:lapis_block': 'pastel:pure_lapis',
+        'pastel:redstone_block': 'pastel:pure_redstone',
+        'pastel:diamond_block': 'pastel:pure_diamond',
+        'pastel:emerald_block': 'pastel:pure_emerald',
+        'pastel:prismarine_block': 'pastel:pure_prismarine',
+        'pastel:quartz_block': 'pastel:pure_quartz',
+        'pastel:glowstone_block': 'pastel:pure_glowstone',
+        'pastel:zinc_block': 'pastel:pure_zinc',
+        'pastel:netherite_scrap_block': 'pastel:pure_netherite_scrap',
+        'pastel:echo_block': 'pastel:pure_echo',
         'pastel:pure_echo': 'minecraft:echo_shard'
     };
 
-    let createRecipesForTag = (tagString, multiplier) => {
-        Ingredient.of(tagString).getItemIds().forEach(inputId => {
-            let outputId = clusterMapping[inputId];
-            
-            if(outputId)
-            {
-                event.shapeless(Item.of(outputId, multiplier), inputId);
-            }
-        });
-    };
 
-    createRecipesForTag('#catalyst:budding_storage', 4);
+    Ingredient.of('#catalyst:budding_storages').getItemIds().forEach(inputId => {
+        let outputId = clusterMapping[inputId];
+        if(outputId)
+        {
+            catalyst.shapeless(Item.of(outputId, 4), inputId).id(`catalyst:buds/${inputId.replace(":", "_")}`);
+        }
+    });
 
     console.log("[CatJS] Added some budding storage recipes")
 });
